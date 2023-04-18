57b563c49e413afd5d035508a3afde7ab1059ad9
Guilherme Godoy <guilhermesachinelli.564@gmail.com>
2023-04-18 13:53:23 -0300

    adsasdassa

---
+++ index.js
@@ -0,0 +1,5 @@
+function triangulo(){
+    let lado1 = Number(document.getElementById("num1").value)
+    let lado2 = Number(document.getElementById("num2").value)
+    let lado3 = Number(document.getElementById("num3").value)
+}
